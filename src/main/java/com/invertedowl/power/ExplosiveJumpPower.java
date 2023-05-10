2f2c5d81f2d3cf0661fdf4370fc0f5b4675915ea
InvertedOwl <commandking123@gmail.com>
2023-05-10 16:25:28 -0700

    lll

--- src/main/java/com/invertedowl/power/ExplosiveJumpPower.java
+++ src/main/java/com/invertedowl/power/ExplosiveJumpPower.java
@@ -6,11 +6,44 @@ import io.github.apace100.apoli.power.PowerType;
 import io.github.apace100.apoli.util.HudRender;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.TntEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
 
 import java.util.function.Consumer;
 
 public class ExplosiveJumpPower extends ActiveCooldownPower {
     public ExplosiveJumpPower(PowerType<?> type, LivingEntity entity, int cooldownDuration, HudRender hudRender, Consumer<Entity> activeFunction) {
         super(type, entity, cooldownDuration, hudRender, activeFunction);
+    }
+
+
+    private boolean onDown = false;
+
+    @Override
+    public void onUse() {
+        if (canUse()) {
+
+            onDown = !onDown;
+
+            PlayerEntity player = (PlayerEntity) entity;
+
+
+            if (onDown) {
+//                boolean mobGriefing = player.getServer().getGameRules().get(GameRules.DO_MOB_GRIEFING).get();
+
+//                player.getServer().getGameRules().get(GameRules.DO_MOB_GRIEFING).set(false, player.getServer());
+//                player.world.createExplosion(player, player.getX(), player.getY() - 1, player.getZ(), 2.0f, World.ExplosionSourceType.MOB);
+//                player.getServer().getGameRules().get(GameRules.DO_MOB_GRIEFING).set(mobGriefing, player.getServer());
+
+                player.setVelocity(player.getVelocity().getX(), 1.2, player.getVelocity().getZ());
+                player.fallDistance = 0;
+
+                player.getWorld().addParticle(ParticleTypes.EXPLOSION_EMITTER, player.getX(), player.getY(), player.getZ(), 0.1, 0.0, 0.0);
+            }
+        }
     }
 }
--- src/main/java/com/invertedowl/registry/RWPowers.java
+++ src/main/java/com/invertedowl/registry/RWPowers.java
@@ -11,6 +11,7 @@ import io.github.apace100.apoli.power.Active;
 import io.github.apace100.apoli.power.Power;
 import io.github.apace100.apoli.power.factory.PowerFactory;
 import io.github.apace100.apoli.registry.ApoliRegistries;
+import io.github.apace100.apoli.util.HudRender;
 import io.github.apace100.calio.data.SerializableData;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
@@ -18,10 +19,14 @@ import net.minecraft.util.Identifier;
 
 public class RWPowers {
     public static final PowerFactory<Power> MESSAGE = new PowerFactory<>(new Identifier(RainWorldOrigins.MOD_ID, "message"), new SerializableData(), data -> (type, entity) -> new TntThrowPower(type, entity)).allowCondition();
-    public static final PowerFactory<Power> EXPLOSIVE_JUMP = new PowerFactory<>(new Identifier(RainWorldOrigins.MOD_ID, "explosive_jump"), new SerializableData(), data -> (type, entity) -> new ExplosiveJumpPower(type, entity)).allowCondition();
+    public static final PowerFactory<Power> EXPLOSIVE_JUMP = new PowerFactory<>(new Identifier(RainWorldOrigins.MOD_ID, "explosive_jump"), new SerializableData().add("key", ApoliDataTypes.BACKWARDS_COMPATIBLE_KEY, new Active.Key()), data -> (type, entity) -> {
+        ExplosiveJumpPower power = new ExplosiveJumpPower(type, entity, 200, HudRender.DONT_RENDER, null);
+        power.setKey((Active.Key)data.get("key"));
+        return power;
+    }).allowCondition();
     public static final PowerFactory<Power> CAN_ONLY_EAT_SPEARED = new PowerFactory<>(new Identifier(RainWorldOrigins.MOD_ID, "can_only_eat_speared"), new SerializableData(), data -> (type, entity) -> new OnlyEatSpeared(type, entity)).allowCondition();
     public static final PowerFactory<Power> NEW_SPEAR = new PowerFactory<>(new Identifier(RainWorldOrigins.MOD_ID, "new_spear"), new SerializableData().add("key", ApoliDataTypes.BACKWARDS_COMPATIBLE_KEY, new Active.Key()), data -> (type, entity) -> {
-        NewSpearPower power = new NewSpearPower(type, entity, 20, null, entity1 -> {entity.sendMessage(Text.of("oo round 2"));});
+        NewSpearPower power = new NewSpearPower(type, entity, 20, HudRender.DONT_RENDER, entity1 -> {entity.sendMessage(Text.of("oo round 2"));});
         power.setKey((Active.Key)data.get("key"));
         return power;
     }).allowCondition();
